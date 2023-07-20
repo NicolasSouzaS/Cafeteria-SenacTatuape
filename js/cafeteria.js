2e15b2a9c80aae85e8adeaf2411a69596f702148
NICOLAS DE SOUZA SANTOS <nicolas.ssantos15@senacsp.edu.br>
2023-07-20 09:49:32 -0300

    Feature: Adição de botão menu e responsividade em todas as páginas

--- js/cafeteria.js
+++ js/cafeteria.js
@@ -1,3 +1,6 @@
 document.getElementsByClassName("btn-mobile")[0].onclick = ()=> {
     document.getElementsByTagName("nav")[0].style.marginLeft = "0px"
 }
+document.getElementsByTagName("main")[0].onclick = ()=>{
+    document.getElementsByTagName("nav")[0].style.marginLeft = "-200px"
+}
